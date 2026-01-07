400cfeb1afa198821fe088b8b7e428dc2d2c90ca
alexperezortuno <alexperezortuno@gmail.com>
2026-01-07 01:08:46 -0300

    refactor CompanyService to use InMemoryCompanyRepository and improve error handling for duplicate tax IDs

--- src/companies/services/company.service.ts
+++ src/companies/services/company.service.ts
@@ -2,21 +2,25 @@ import {CompanyRepositoryPort} from "../domain/ports/company.repository.port";
 import {CreateCompanyDto} from "../dtos/create-company.dto";
 import {Corporate} from "../domain/entities/corporate.entity";
 import {Pyme} from "../domain/entities/pyme.entity";
+import {InMemoryCompanyRepository} from "../infrastructure/persistence/in-memory/company.repository";
+import {randomUUID} from "node:crypto";
+import {HttpException, HttpStatus} from "@nestjs/common";
 
 export class CompanyService {
     constructor(
         private readonly companyRepository: CompanyRepositoryPort
     ) {
+        this.companyRepository = new InMemoryCompanyRepository();
     }
 
-    async create(dto: CreateCompanyDto) {
+    async create(dto: CreateCompanyDto): Promise<Corporate | Pyme | Error> {
         const taxId: string = dto.taxId.replace(/\D/g, '');
 
         if (await this.companyRepository.findByTaxId(taxId)) {
-            throw new Error('Tax ID already exists');
+            throw new HttpException('Tax ID already exists', HttpStatus.CONFLICT);
         }
 
-        const id = crypto.randomUUID();
+        const id = randomUUID();
         let company: Corporate | Pyme = new Pyme(
             id,
             dto.name,
