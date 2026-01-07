1f90e4875ab24d06861a0d3f3da9effc769ca1d9
alexperezortuno <alexperezortuno@gmail.com>
2026-01-06 23:55:41 -0300

    add CompaniesModule with CompanyController and CompanyService

--- src/companies/companies.module.ts
+++ src/companies/companies.module.ts
@@ -1,4 +1,10 @@
-import { Module } from '@nestjs/common';
+import {Module} from '@nestjs/common';
+import {CompanyController} from "./infrastructure/controllers/company.controller";
+import {CompanyService} from "./services/company.service";
 
-@Module({})
-export class CompaniesModule {}
+@Module({
+    controllers: [CompanyController],
+    providers: [CompanyService],
+})
+export class CompaniesModule {
+}
