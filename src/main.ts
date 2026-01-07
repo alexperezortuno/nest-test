a4ad1b2b21d46b165bc21dfd4554970fc46f7aba
alexperezortuno <alexperezortuno@gmail.com>
2026-01-06 22:03:27 -0300

    add validation pipes and cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {INestApplication, ValidationPipe} from "@nestjs/common";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app: INestApplication = await NestFactory.create(AppModule);
+    const port: number = Number(process.env.PORT) || 3000;
+
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true,
+            transform: true,
+            forbidNonWhitelisted: true,
+        }),
+    );
+
+    app.enableCors(
+        {origin: '*'},
+    );
+
+    await app.listen(port);
+    console.log(`Application is running on: ${await app.getUrl()}`);
 }
+
 bootstrap();
