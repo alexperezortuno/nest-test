2f4769794983dc30c62324831fdf58d5f134087f
alexperezortuno <alexperezortuno@gmail.com>
2026-01-07 01:08:34 -0300

    refactor CompanyController to use async/await for create method

--- src/companies/infrastructure/controllers/company.controller.ts
+++ src/companies/infrastructure/controllers/company.controller.ts
@@ -1,13 +1,16 @@
-import {Body, Controller, Post} from "@nestjs/common";
+import {Body, Controller, HttpException, Post} from "@nestjs/common";
 import {CreateCompanyDto} from "../../dtos/create-company.dto";
 import {CompanyService} from "../../services/company.service";
+import {Company} from "../../domain/entities/company.entity";
+import {Corporate} from "../../domain/entities/corporate.entity";
+import {Pyme} from "../../domain/entities/pyme.entity";
 
 @Controller('companies')
 export class CompanyController {
     constructor(private readonly service: CompanyService) {}
 
     @Post('create')
-    create(@Body() dto: CreateCompanyDto) {
-        return this.service.create(dto);
+    async create(@Body() dto: CreateCompanyDto): Promise<Corporate | Pyme | Error> {
+        return await this.service.create(dto);
     }
 }
