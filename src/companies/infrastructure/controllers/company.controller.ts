4e606eb20d087e464d94b57c28355d6a4a440222
alexperezortuno <alexperezortuno@gmail.com>
2026-01-08 00:25:16 -0300

    feat: update create method to return structured response and add endpoint for retrieving companies with transfers

--- src/companies/infrastructure/controllers/company.controller.ts
+++ src/companies/infrastructure/controllers/company.controller.ts
@@ -1,16 +1,17 @@
-import {Body, Controller, Delete, Get, HttpException, Param, Post} from "@nestjs/common";
+import {Body, Controller, Delete, Get, HttpException, Param, Post, Query} from "@nestjs/common";
 import {CreateCompanyDto} from "../../dtos/create-company.dto";
 import {CompanyService} from "../../services/company.service";
 import {Corporate} from "../../domain/entities/corporate.entity";
 import {Pyme} from "../../domain/entities/pyme.entity";
 import {DefaultResponseDto} from "../../../shared/utils/dtos/default-response.dto";
+import {ResponseCompanyDto} from "../../dtos/response-company.dto";
 
 @Controller('companies')
 export class CompanyController {
     constructor(private readonly service: CompanyService) {}
 
     @Post('create')
-    async create(@Body() dto: CreateCompanyDto): Promise<Corporate | Pyme | HttpException> {
+    async create(@Body() dto: CreateCompanyDto): Promise<ResponseCompanyDto | HttpException> {
         return await this.service.create(dto);
     }
 
@@ -27,5 +28,14 @@ export class CompanyController {
     @Delete("delete/:id")
     async delete(@Param('id') id: string): Promise<DefaultResponseDto | HttpException> {
         return await this.service.delete(id);
+    }
+
+    @Get('transfers/last-month')
+    getCompaniesWithTransfers(
+        @Query('days') days?: string,
+    ) {
+        const parsedDays = days ? Number(days) : 30;
+
+        return this.service.getCompaniesWithTransfersLastMonth(parsedDays);
     }
 }
