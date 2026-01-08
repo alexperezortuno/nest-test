aa3485b24a5017fb284b39ff731734ff85b9ced1
alexperezortuno <alexperezortuno@gmail.com>
2026-01-07 23:06:29 -0300

    feat: implement create, update, delete, and existence check methods in CompanyRepositoryPort

--- src/companies/infrastructure/persistence/in-memory/company.repository.ts
+++ src/companies/infrastructure/persistence/in-memory/company.repository.ts
@@ -1,6 +1,9 @@
 import {Company} from "../../../domain/entities/company.entity";
 import {Injectable} from "@nestjs/common";
 import {CompanyRepositoryPort} from "../../../domain/ports/company.repository.port";
+import {Pyme} from "../../../domain/entities/pyme.entity";
+import {Corporate} from "../../../domain/entities/corporate.entity";
+import {response} from "express";
 
 
 @Injectable()
@@ -8,20 +11,43 @@ export class InMemoryCompanyRepository implements CompanyRepositoryPort {
     private companies: Map<string, Company> = new Map();
 
     async findByTaxId(taxId: string): Promise<Company | null> {
-        const normalized = taxId.replace(/\D/g, "");
         for (const company of this.companies.values()) {
-            if ((company.taxId ?? "").replace(/\D/g, "") === normalized) {
+            if (company.taxId === taxId) {
                 return company;
             }
         }
         return null;
     }
 
-    findByIds(ids: string[]): Promise<Company[]> {
-        throw new Error("Method not implemented.");
+    async create(company: Company): Promise<void> {
+        this.companies.set(company.id, company);
     }
 
-    async save(company: Company): Promise<void> {
+    async findAllPymes(): Promise<Pyme[]> {
+        return Array.from(this.companies.values()).filter(company => company instanceof Pyme);
+    }
+
+    async findAllCorporates(): Promise<Corporate[]> {
+        return Array.from(this.companies.values()).filter(company => company instanceof Corporate);
+    }
+
+    async delete(taxId: string): Promise<boolean> {
+        let c: Company = Array.from(this.companies.values()).find(
+            company => company.taxId === taxId
+        );
+        this.companies.delete(c.id);
+        return true;
+    }
+
+    async update(company: Corporate | Pyme): Promise<Corporate | Pyme> {
         this.companies.set(company.id, company);
+        return company;
+    }
+
+    async exists(taxId: string): Promise<boolean> {
+        if (!taxId) return false;
+        return Array.from(this.companies.values()).some(
+            company => company.taxId === taxId
+        );
     }
 }
