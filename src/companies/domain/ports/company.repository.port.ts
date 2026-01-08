6effc365c869bb2713d298ce33b7312efe7c57a5
alexperezortuno <alexperezortuno@gmail.com>
2026-01-07 23:06:23 -0300

    feat: expand CompanyRepositoryPort with new methods for creating, updating, and deleting companies

--- src/companies/domain/ports/company.repository.port.ts
+++ src/companies/domain/ports/company.repository.port.ts
@@ -1,10 +1,19 @@
 import {Company} from "../entities/company.entity";
 import {Pyme} from "../entities/pyme.entity";
+import {Corporate} from "../entities/corporate.entity";
 
 export interface CompanyRepositoryPort {
+    create(company: Company | Pyme): Promise<void>;
+
     findByTaxId(taxId: string): Promise<Company | null>;
 
-    save(company: Company | Pyme): Promise<void>;
+    findAllPymes(): Promise<Pyme[]>;
 
-    findByIds(ids: string[]): Promise<Company[]>;
+    findAllCorporates(): Promise<Corporate[]>;
+
+    exists(taxId: string): Promise<boolean>;
+
+    update(company: Corporate | Pyme): Promise<Corporate | Pyme>;
+
+    delete(id: string): Promise<boolean>;
 }
