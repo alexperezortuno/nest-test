73017ca81a54312fe60c4a28e620e4d4a762c773
alexperezortuno <alexperezortuno@gmail.com>
2026-01-07 23:06:35 -0300

    feat: add methods for retrieving and deleting companies in CompanyService

--- src/companies/services/company.service.ts
+++ src/companies/services/company.service.ts
@@ -5,15 +5,17 @@ import {Pyme} from "../domain/entities/pyme.entity";
 import {InMemoryCompanyRepository} from "../infrastructure/persistence/in-memory/company.repository";
 import {randomUUID} from "node:crypto";
 import {HttpException, HttpStatus} from "@nestjs/common";
+import {DefaultResponseDto} from "../../shared/utils/dtos/default-response.dto";
 
 export class CompanyService {
     constructor(
         private readonly companyRepository: CompanyRepositoryPort
     ) {
+        // Only for testing purposes
         this.companyRepository = new InMemoryCompanyRepository();
     }
 
-    async create(dto: CreateCompanyDto): Promise<Corporate | Pyme | Error> {
+    async create(dto: CreateCompanyDto): Promise<Corporate | Pyme | HttpException> {
         const taxId: string = dto.taxId.replace(/\D/g, '');
 
         if (await this.companyRepository.findByTaxId(taxId)) {
@@ -45,8 +47,39 @@ export class CompanyService {
             )
         }
 
-        await this.companyRepository.save(company);
+        await this.companyRepository.create(company);
 
         return company;
+    }
+
+    async findAllPyme(): Promise<Pyme[] | HttpException> {
+        const r: Pyme[] = await this.companyRepository.findAllPymes();
+        if (r.length > 0) return r;
+        throw new HttpException('No companies found', HttpStatus.NO_CONTENT);
+    }
+
+    async findAllCorporate(): Promise<Corporate[] | HttpException> {
+        const r: Corporate[] = await this.companyRepository.findAllCorporates();
+        if (r.length > 0) return r;
+        throw new HttpException('No companies found', HttpStatus.NO_CONTENT);
+    }
+
+    async delete(taxId: string): Promise<any | HttpException> {
+        if (!taxId) {
+            return Promise.resolve(new HttpException('Invalid company ID', HttpStatus.BAD_REQUEST));
+        }
+
+        const company = await this.companyRepository.exists(taxId);
+        if (!company) {
+            throw new HttpException('Company not found', HttpStatus.NOT_FOUND)
+        }
+
+        return await this.companyRepository.delete(taxId)
+            .then(() => {
+                return new DefaultResponseDto('Company deleted successfully');
+            })
+            .catch(() => {
+                throw new HttpException('Company not found', HttpStatus.NOT_FOUND);
+            });
     }
 }
