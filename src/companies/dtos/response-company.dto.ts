69fbe896812b1a4a5914c3d597b91df82bc69922
alexperezortuno <alexperezortuno@gmail.com>
2026-01-08 00:25:37 -0300

    feat: extend ResponseCompanyDto to include additional fields and validation

--- src/companies/dtos/response-company.dto.ts
+++ src/companies/dtos/response-company.dto.ts
@@ -1,16 +1,55 @@
 import {CompanyType} from "../domain/value-objects/company-type.vo";
 import {StatusType} from "../domain/value-objects/status-type.vo";
 import {LocationId} from "../domain/value-objects/location-id.vo";
+import {IsEnum, IsNumber, IsString} from "class-validator";
+import {Company} from "../domain/entities/company.entity";
 
 export class ResponseCompanyDto {
+
+    constructor(company: Company) {
+        this.id = company.id;
+        this.name = company.name;
+        this.taxId = company.taxId;
+        this.email = company.email;
+        this.phone = company.phone;
+        this.address = company.Address;
+        this.locationId = company.locationId;
+        this.status = company.status;
+        this.type = company.type;
+        this.employeesCount = (company as any).employeesCount;
+        this.capital = (company as any).capital;
+    }
+
+    @IsString()
     id: string;
+
+    @IsString()
     name: string;
+
+    @IsString()
     taxId: string;
+
+    @IsString()
     email: string;
+
+    @IsString()
     phone: string;
+
+    @IsString()
     address: string;
+
+    @IsString()
     locationId: LocationId;
+
+    @IsEnum(StatusType)
     status: StatusType;
+
+    @IsEnum(CompanyType)
     type: CompanyType;
+
+    @IsNumber()
     employeesCount?: number;
+
+    @IsNumber()
+    capital?: number;
 }
